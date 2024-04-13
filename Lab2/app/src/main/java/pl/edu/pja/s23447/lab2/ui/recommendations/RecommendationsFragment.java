f9e33bef1b43bca38e7795d6bce50ffa67a21f00
Damian Kreft <s23447@pjwstk.edu.pl>
2024-04-13 14:21:20 +0200

    Add Lab2 exercise.

---
+++ Lab2/app/src/main/java/pl/edu/pja/s23447/lab2/ui/recommendations/RecommendationsFragment.java
@@ -0,0 +1,34 @@
+package pl.edu.pja.s23447.lab2.ui.recommendations;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import pl.edu.pja.s23447.lab2.R;
+
+public class RecommendationsFragment extends Fragment {
+
+    public static RecommendationsFragment newInstance() {
+        return new RecommendationsFragment();
+    }
+
+    private RecommendationsViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recommendations, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RecommendationsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
